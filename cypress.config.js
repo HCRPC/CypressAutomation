1ffb5159b15fb6e385650436ceb683625552c789
HCRPC <haciarpaci12@gmail.com>
2023-06-23 10:12:43 +0300

    new commits

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl :'https://practice.cydeo.com/', // base burda tanımlanıyor kalan kısmı url in devamı test içinde 
+    video :false,
+    retries: 0, //if it 2 , after fails it should run extra 2 times 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -14,11 +14,32 @@ describe('Find or Get by Using Different Locators',()=>{
         // attribute name and value     
         cy.get("[type = 'text']").clear();// clear what is typed
 
+        //tag name
         cy.get("input").each((item, index, list)=>{
             // assert the lengt is equal to 2
-            expect(list).to.have.length(2);
+            expect(list).to.have.length(2); // explicir assertion
             expect(item).to.have.attr("type");
         })
+
+        //attribute
+        cy.get('[type]')
+
+        ///by class name 
+        cy.get('.btn.btn-primary')
+
+        // by id
+       //  cy.get('wooden_spoon')//hash işaretini koyamıyoyum !!!!! wooden in basında olmalı
+
+        //to use text in cypress, we have no xpath
+          cy.get('button').should('contain','Login').click();   
+        
+          
+    })
+    it.only(('Check finding elements bu traveling through DOM'),()=>{
+
+        // travel to find login button go parent 
+        cy.get('input[name="username"]').parents('form').find('button').should('contain','Login').click();
+
 
     })
 
